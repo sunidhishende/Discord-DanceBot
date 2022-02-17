e630ece8a2e0b3ce0bd002c445408c6e70ffa895
Sunidhi Shende <sunidhi.shende1@gmail.com>
2022-02-17 23:32:36 +0530

    added help command

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from collections import Counter
+from hashlib import sha256
+from aiohttp import Payload
+from black import target_version_option_callback
 import discord
 import random
 from discord.embeds import Embed
@@ -11,16 +15,64 @@ import pafy
 
 # Client = our bot
 client= commands.Bot(command_prefix='++')
+client.remove_command('help')
+intents= discord.Intents.default()
+intents.members= True
+intents.reactions= True
 
 # Functionality of the bot
 
 @client.event
+async def on_ready():
+    print('Bot says hi')
+
+@client.event
 async def on_message(message):
+    if message.author==client.user:
+        return
     if message.content.startswith('Hello'):
         responses= ['Hi!', 'How you doin?', 'Hello Friend!', 'Hey There!']
         await message.channel.send(random.choice(responses))
     await client.process_commands(message)
 
+@client.command(name='help')
+async def help(ctx):
+    embed=discord.Embed(
+        title='Bot Commands',
+        description='Welcome to the help section. Here are all the commands!',
+        color= discord.Colour.green())
+    embed.add_field(
+        name='++help',
+        value='List of all the commands',
+        inline='false'
+    )
+    embed.add_field(
+        name='++pair',
+        value='Pairs up the given names randomly',
+        inline='false'
+    )
+    embed.add_field(
+        name='++join',
+        value='Bot joins the voice channel',
+        inline='false'
+    )
+    embed.add_field(
+        name='++exit',
+        value='Bot exits the voice channel',
+        inline='false'
+    )
+    embed.add_field(
+        name='++beginbattle',
+        value='Plays the songs mentioned and dance battle starts',
+        inline='false'
+    )
+    embed.add_field(
+        name='++vote',
+        value='Anonymous voting to decide the winner',
+        inline='false'
+    )
+    await ctx.send(embed=embed)
+    
 
 #pair people command
 
@@ -34,9 +86,15 @@ async def pair(ctx):
     inp = [(inp) for inp in resp.split(" ")]
     result= []
     random.shuffle(inp)
-    for j in range(0, len(inp), 2):
-        result= [inp[j], inp[j+1]]
-        await ctx.message.channel.send(' '.join(result))
+    if len(inp)%2==0:
+        for j in range(0, len(inp), 2):
+            result= [inp[j], inp[j+1]]
+            await ctx.message.channel.send(' '.join(result))
+    if len(inp)%2!=0:
+        for j in range(0, len(inp)-1, 2):
+            result= [inp[j], inp[j+1]]
+            await ctx.message.channel.send(' '.join(result))
+        await ctx.message.channel.send(inp[len(inp)-1])
         
 
 #Join command
@@ -78,11 +136,11 @@ async def beginbattle(ctx):
           url_suffix = result['url_suffix']
           link = "https://www.youtube.com" + url_suffix
           playlist.append(link)
-
+    
+    
     for j in range(0, len(playlist),1):
-        
+        #await ctx.channel.send('Beginning round {}'.format(j+1))
         voice = get(client.voice_clients, guild=ctx.guild)
-        await ctx.channel.send('Beginning round {}'.format(j+1))
         url= pafy.new(playlist[j])
         audio = url.getbestaudio()
         source = discord.FFmpegPCMAudio(audio.url)
@@ -100,18 +158,49 @@ async def beginbattle(ctx):
 #Voting command
 
 numbers=["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"]
-
+targetmsgid=''
+Dict={}
 @client.command(name= 'vote')
-async def vote(ctx, *options):
+async def vote(ctx):
+    await ctx.send("Give names to vote on with spaces in between them to distinguish them")
+    def check(m):
+        return m.author.id == ctx.author.id
+    response= await client.wait_for('message', check=check)
+    resp= response.content
+    inp = [(inp) for inp in resp.split(" ")]
     emb= Embed(Title= "Vote")
-    fields=[("Who won?", "\n".join([f"{numbers[idx]} {options[idx]}" for idx in range(len(options)) ]), False)]
+    fields=[("Who won?", "\n".join([f"{numbers[idx]} {inp[idx]}" for idx in range(len(inp)) ]), False)]
     for name, value, inline in fields:
         emb.add_field(name=name, value=value, inline=inline)
     message= await ctx.send(embed=emb)
-    for emoji in numbers[: len(options)]:
+    for emoji in numbers[: len(inp)]:
         await message.add_reaction(emoji)
+    targetmsgid=message.id
+    time.sleep(5)
+    res=Counter(Dict.values())
+    max=1
+    winner=''
+    for key,value in res.items():
+        if value>=max:
+            max=value
+            winner=key
+        
+    await ctx.send("The winner is "+ winner)
 
-             
+
+async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+    userid=payload.user_id
+    if payload.message_id==targetmsgid:
+        Dict.update(userid='{payload.emoji}')
+    targetmsgid.remove(payload.user_id)
+
+        
+        
+       
+        
+    
+    
+    
 
     
 
@@ -120,4 +209,5 @@ async def vote(ctx, *options):
 
 
 # Run the bot on the server
-client.run(#'Enter your token here')
+# client.run(#'Enter your token here')
+client.run('ODQ1NTM0MTkxMzIwMDM5NDM1.YKiXFA.JMaT073jsG6mxhjqRhpug-rfZIg')
